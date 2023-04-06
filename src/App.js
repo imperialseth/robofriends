e1829fa410b7ceea723ce896ebaf5fca4c067951
mtopp <madstopp@gmail.com>
2023-04-06 23:27:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const state = {
+// 	robots: robots,
+// 	searchfield: "",
+// }
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ""
+		}
+	}
+
+componentDidMount() {
+	fetch("https://jsonplaceholder.typicode.com/users")
+	.then(response => response.json())
+	.then(users => this.setState({robots: users}));
+}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+	}
+
+	render () {
+		const { robots, searchfield} = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+	if (!robots.length) {
+		return <h1> Loading ... </h1>
+	} else {
+			return (
+				<div className="tc">
+					<h1 className="f1">Automated Friends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
+		}		
+	}
 }
 
 export default App;
